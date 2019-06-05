a75269b454aa71c958940f5018b510cc4af1e93b
Michael Jennings <michael.jennings91@gmail.com>
2019-06-05 14:28:12 +0100

    Update options

--- src/Commands/MigrateMakeCommand.php
+++ src/Commands/MigrateMakeCommand.php
@@ -14,9 +14,11 @@ class MigrateMakeCommand extends BaseMigrateMakeCommand
      * @var string
      */
     protected $signature = 'migration {name : The name of the migration.}
-        {--create= : The table to be created.}
-        {--table= : The table to migrate.}
-        {--path= : The location where the migration file should be created.}';
+        {--create= : The table to be created}
+        {--table= : The table to migrate}
+        {--path= : The location where the migration file should be created}
+        {--realpath : Indicate any provided migration file paths are pre-resolved absolute paths}
+        {--fullpath : Output the full path of the migration}';
 
     /**
      * Get migration path (either specified by '--path' option or default location).
